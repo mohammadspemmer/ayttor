3099dcdc228777112a96a9fe1ce4ddc489e687c7
AYTOR <30493811+mohammadspemmer@users.noreply.github.com>
2017-10-10 06:37:43 -0700

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,31 @@
+do local _ = {
+  admins = {
+    {
+      444819696,
+      "@Dasti\\_Typi\\_Kal\\_kon"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun",
+    "del",
+    "mo",
+    "dal1",
+    "ping"
+  },
+  info_text = "》Beyond Reborn v6.0\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn \n\n》Admins :\n》@SoLiD ➣ Founder & Developer《\n》@Makan ➣ Developer《\n》@ToOfan ➣ Developer《\n\n》Special thanks to :\n》Beyond Team Members\n\n》Our channel :\n》@BeyondTeam《\n\n》Our website :\n》http://Beyond-Dev.iR\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    431714063,
+    0
+  }
+}
+return _
+end
